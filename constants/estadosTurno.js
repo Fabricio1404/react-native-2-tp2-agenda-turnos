8e66bb64b1bd4be69e2f9cf94c4a521f69b1c3c0
Fabricio Augusto <augustofabry@gmail.com>
2026-08-14 14:25:11 -0300

    T004-T005 - constants de estados y catalogo de servicios

---
+++ constants/estadosTurno.js
@@ -0,0 +1,8 @@
+export const ESTADOS_TURNO = {
+  PENDIENTE: 'pendiente',
+  CONFIRMADO: 'confirmado',
+  COMPLETADO: 'completado',
+  CANCELADO: 'cancelado',
+};
+
+export const ESTADOS_TURNO_LISTA = Object.values(ESTADOS_TURNO);
---
+++ constants/servicios.js
@@ -0,0 +1,5 @@
+export const SERVICIOS = [
+  { id: '1', nombre: 'Corte' },
+  { id: '2', nombre: 'Barba' },
+  { id: '3', nombre: 'Corte + Barba' },
+];
